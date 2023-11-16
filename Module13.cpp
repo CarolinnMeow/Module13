c685f1a7990af0375abc5576b1e3faa643e155bb
aleksandra_orlova <aleksa.orlik2000@gmail.com>
2023-11-16 16:30:13 +0300

    HW done

---
+++ Module13.cpp
@@ -0,0 +1,13 @@
+﻿// Module13.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include "Helpers.h"
+
+using namespace std;
+
+int main()
+{
+    cout << SquareOfSum(10,20);
+}
+
